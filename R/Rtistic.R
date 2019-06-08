e9113ea790bc94460e0568da1b317274cf137227
Emily Riederer <emilyriederer@gmail.com>
2019-06-08 07:44:42 -0500

    added pkg level docs

---
+++ R/Rtistic.R
@@ -0,0 +1,10 @@
+#' \code{Rtistic} package
+#'
+#' A collection of palettes and themes for plots, documents, and slides
+#'
+#' See the README on
+#' \href{https://github.com/emilyriederer/Rtistic}{GitHub}
+#'
+#' @docType package
+#' @name Rtistic
+NULL
